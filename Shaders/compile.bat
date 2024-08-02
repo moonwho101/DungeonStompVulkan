a1f75965b387865c5fff8371c9cd90c4879911e3
Mark Longo <mlongo@aptisense.com>
2024-08-02 12:03:11 -0400

    Dungeon Stomp Vulkan

---
+++ Shaders/compile.bat
@@ -0,0 +1,1 @@
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe %1 -o %1.spv
---
+++ Shaders/compileall.bat
@@ -0,0 +1,5 @@
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe debug.frag -o debug.frag.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe default.frag -o default.frag.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe shadow.frag -o shadow.frag.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe sky.frag -o sky.frag.spv
+
