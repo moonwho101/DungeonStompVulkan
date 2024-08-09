0e8ad6ae48109d7b70230982c87bbdf3d0ed5de8
Mark Longo <mlongo@aptisense.com>
2024-08-09 15:12:40 -0400

    Dungeon Stomp Vulkan

--- Common/Vulkan.cpp
+++ Common/Vulkan.cpp
@@ -1367,7 +1367,7 @@ namespace Vulkan {
 			//all colors, no blending
 			VkPipelineColorBlendAttachmentState blendAttachment{};
 			blendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
-			blendAttachment.blendEnable = pipelineInfo.blend;
+			blendAttachment.blendEnable = true;
 			//blendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_COLOR;
 			//blendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
 			//blendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
