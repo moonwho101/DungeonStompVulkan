e9de9b692d6b0f79b18c5caeb74ba62f1d85855f
Mark Longo <mlongo@aptisense.com>
2024-12-21 12:03:27 -0500

    Dungeon Stomp Vulkan Surface Normals

--- src/ProcessModel.cpp
+++ src/ProcessModel.cpp
@@ -886,7 +886,7 @@ void SmoothNormals(int start_cnt) {
 
 			scount = 0;
 
-			for (int j = start_cnt; j < cnt; j++) {
+			for (int j = i; j < cnt; j++) {
 				if (tracknormal[j] == 0 && x == src_v[j].x && y == src_v[j].y && z == src_v[j].z) {
 					//found shared vertex
 					sharedv[scount] = j;
