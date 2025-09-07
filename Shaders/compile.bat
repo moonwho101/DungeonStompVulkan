7d0ddc367e0c146bcbe11d36dfa7e979185f4644
Mark Longo <mlongo@aptisense.com>
2025-09-07 14:09:37 -0400

    Dungeon Stomp Vulkan rim lighting

--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -1,1 +1,5 @@
-C:\VulkanSDK\1.3.250.1\Bin\glslc.exe %1 -o %1.spv
+C:\VulkanSDK\1.4.313.1\Bin\glslc.exe debug.frag -o debug.frag.spv
+C:\VulkanSDK\1.4.313.1\Bin\glslc.exe default.frag -o default.frag.spv
+C:\VulkanSDK\1.4.313.1\Bin\glslc.exe shadow.frag -o shadow.frag.spv
+C:\VulkanSDK\1.4.313.1\Bin\glslc.exe sky.frag -o sky.frag.spv
+
--- Shaders/compileall.bat
+++
@@ -1,5 +0,0 @@
-C:\VulkanSDK\1.4.313.1\Bin\glslc.exe debug.frag -o debug.frag.spv
-C:\VulkanSDK\1.4.313.1\Bin\glslc.exe default.frag -o default.frag.spv
-C:\VulkanSDK\1.4.313.1\Bin\glslc.exe shadow.frag -o shadow.frag.spv
-C:\VulkanSDK\1.4.313.1\Bin\glslc.exe sky.frag -o sky.frag.spv
-
